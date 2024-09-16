c9c8623daf4f9c08da3283e36443e756ff2cb64e
Dima <mazafon64@gmail.com>
2024-09-16 15:12:24 +0300

    added the email text file and printed it in the console

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ from sklearn import svm
 from process_email import process_email
 from process_email import email_features
 from process_email import get_dictionary
+
+def email_text():
+    email = ''
+    with open('email.txt', 'r') as file:
+        email = file.read().replace('\n', '')
+    return email
+
+def main():
+    email = email_text()
+    print(email)
+
+if __name__ == '__main__':
+    main()
