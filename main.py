70c3222781d81c3e109707363f7f09d9a8525a70
Dima <mazafon64@gmail.com>
2024-09-16 15:05:55 +0300

    connecting lib

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy.io
+from sklearn import svm
+
+from process_email import process_email
+from process_email import email_features
+from process_email import get_dictionary
